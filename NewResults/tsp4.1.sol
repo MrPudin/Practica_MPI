708b32a55d7f8f080c724a4b7e3b818d1d38bdad
MrPudin <daranutacristian60@gmail.com>
2026-05-13 14:19:17 +0200

    Initial Commit

---
+++ NewResults/tsp4.1.sol
@@ -0,0 +1,8 @@
+Recorrido: <0,1> <1,3> <2,0> <3,2>   Coste(ci): 16
+
+Recorrido: <0,2> <1,3> <2,1> <3,0>   Coste(ci): 15
+
+Recorrido: <0,1> <1,2> <2,3> <3,0>   Coste(ci): 9
+
+Tiempo de ejecución: 5.6e-05 
+
---
+++ NewResults/tsp5.1.sol
@@ -0,0 +1,12 @@
+Recorrido: <0,4> <1,3> <2,0> <3,2> <4,1>   Coste(ci): 40
+
+Recorrido: <0,3> <1,0> <2,1> <3,4> <4,2>   Coste(ci): 40
+
+Recorrido: <0,1> <1,3> <2,0> <3,4> <4,2>   Coste(ci): 37
+
+Recorrido: <0,3> <1,2> <2,0> <3,4> <4,1>   Coste(ci): 36
+
+Recorrido: <0,3> <1,4> <2,0> <3,1> <4,2>   Coste(ci): 28
+
+Tiempo de ejecución: 0.000312 
+
